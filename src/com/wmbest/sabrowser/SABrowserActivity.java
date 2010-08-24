6744c8a3432a9686a6d3d2f79c094a63823e1b8d
Bill Best <wmbest2@gmail.com>
2010-08-24 11:17:12 -0400

    Initial commit

---
+++ src/com/wmbest/sabrowser/SABrowserActivity.java
@@ -0,0 +1,15 @@
+package com.wmbest.sabrowser;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class SABrowserActivity extends Activity
+{
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
