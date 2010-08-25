ec4608dd874b5680f690a653f90169ee465880eb
Bill Best <wmbest2@gmail.com>
2010-08-24 23:08:32 -0400

    http request experiment

--- src/com/wmbest/sabrowser/SABrowserActivity.java
+++ src/com/wmbest/sabrowser/SABrowserActivity.java
@@ -2,6 +2,7 @@ package com.wmbest.sabrowser;
 
 import android.app.Activity;
 import android.os.Bundle;
+import org.apache.http.*;
 
 public class SABrowserActivity extends Activity
 {
@@ -11,5 +12,45 @@ public class SABrowserActivity extends Activity
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
+
+		String websiteData = null;
+
+		try {
+
+			DefaultHttpClient client = new DefaultHttpClient();
+			URI u = new URI("http://forums.somethingawful.com");
+			HttpGet method = new HttpGet(uri);
+			HttpResponse res = client.execute(method);
+
+			InputStream data = res.getEntity().getContent();
+			websiteData = generateString(data);
+
+			TextView tx = (TextView)findViewById(R.id.output);
+			tx.setText(websiteData);
+		} catch (ClientProtocolException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		} catch (URISyntaxException e) {
+			e.printStackTrace();
+		}
     }
+
+	public String generateString(InputStream stream) {
+		InputStreamReader reader = new InputStreamReader(stream);
+		BufferedReader buffer = new BufferedReader(reader);
+		StringBuilder sb = new StringBuilder();
+
+		try {
+			String cur;
+			while ((cur = buffer.readLine()) != null) {
+				sb.append(cur + "\n");
+			}
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+
+		return sb.toString();
+	}
+
 }
