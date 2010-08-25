4ce3955fa25a943dbe0a3c492a07e2484426e912
Bill Best <wmbest2@gmail.com>
2010-08-25 09:43:18 -0400

    Changed URI to try to diagnose connection problems

--- src/com/wmbest/sabrowser/SABrowserActivity.java
+++ src/com/wmbest/sabrowser/SABrowserActivity.java
@@ -2,14 +2,28 @@ package com.wmbest.sabrowser;
 
 import android.app.Activity;
 import android.os.Bundle;
-import org.apache.http.*;
+import android.widget.TextView;
+import org.apache.http.impl.client.DefaultHttpClient;
+import java.net.URI;
+import java.net.URISyntaxException;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.client.ClientProtocolException;
+import org.apache.http.HttpResponse;
+import java.io.BufferedReader;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.IOException;
+import android.util.Log;
 
 public class SABrowserActivity extends Activity
 {
+
+	private static final String TAG = "SABrowserActivity";
     /** Called when the activity is first created. */
     @Override
     public void onCreate(Bundle savedInstanceState)
     {
+		Log.d(TAG, "Entered onCreate");
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
 
@@ -18,12 +32,14 @@ public class SABrowserActivity extends Activity
 		try {
 
 			DefaultHttpClient client = new DefaultHttpClient();
-			URI u = new URI("http://forums.somethingawful.com");
+			URI uri = new URI("http://www.google.com/");
 			HttpGet method = new HttpGet(uri);
 			HttpResponse res = client.execute(method);
-
+			Log.d(TAG, "Created Objects, Now Creating Stream");
 			InputStream data = res.getEntity().getContent();
 			websiteData = generateString(data);
+
+			Log.d(TAG, "Generated String: " + websiteData);
 
 			TextView tx = (TextView)findViewById(R.id.output);
 			tx.setText(websiteData);
@@ -37,6 +53,7 @@ public class SABrowserActivity extends Activity
     }
 
 	public String generateString(InputStream stream) {
+		Log.d(TAG, "generateString");
 		InputStreamReader reader = new InputStreamReader(stream);
 		BufferedReader buffer = new BufferedReader(reader);
 		StringBuilder sb = new StringBuilder();
